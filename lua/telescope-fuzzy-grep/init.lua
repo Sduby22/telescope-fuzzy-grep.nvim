7a727f885e4f901bd01255710239671a60e787e5
Sduby22 <supersduby@gmail.com>
2023-07-24 17:29:47 +0800

    style: format

--- lua/telescope-fuzzy-grep/init.lua
+++ lua/telescope-fuzzy-grep/init.lua
@@ -1,27 +1,27 @@
 local pickers = require("telescope.pickers")
-local actions = require "telescope.actions"
-local make_entry = require "telescope.make_entry"
+local actions = require("telescope.actions")
+local make_entry = require("telescope.make_entry")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 
 local M = {}
 local flatten = vim.tbl_flatten
 
-local function M.fuzzy_grep(opts)
+function M.fuzzy_grep(opts)
 	local opts = opts or {}
-  local vimgrep_arguments = opts.vimgrep_arguments or conf.vimgrep_arguments
+	local vimgrep_arguments = opts.vimgrep_arguments or conf.vimgrep_arguments
 
-  local args = vimgrep_arguments
+	local args = vimgrep_arguments
 
-  local live_grepper = finders.new_job(function(prompt)
-    if not prompt or prompt == "" then
-      return nil
-    end
+	local live_grepper = finders.new_job(function(prompt)
+		if not prompt or prompt == "" then
+			return nil
+		end
 
-    local search_list = {}
+		local search_list = {}
 
-    return flatten { args, "--", prompt }
-  end, make_entry.gen_from_vimgrep(opts), opts.max_results, opts.cwd)
+		return flatten({ args, "--", prompt })
+	end, make_entry.gen_from_vimgrep(opts), opts.max_results, opts.cwd)
 
 	pickers
 		.new(opts, {
